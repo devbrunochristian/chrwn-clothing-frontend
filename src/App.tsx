c17137e299772c598abb6e0d26596df32e41fd94
Bruno Christian Da Silva <devbrunochristian@gmail.com>
2021-07-04 12:23:04 -0300

    add HeaderComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import Header from 'components/header-component/Header'
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage'
+import './App.styles.scss'
 
 function App(): React.ReactElement {
     return (
-        <Switch>
-            <div>
+        <div className="container">
+            <Header />
+            <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
-            </div>
-        </Switch>
+            </Switch>
+        </div>
     )
 }
 
